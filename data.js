1dcdd9784f48caca0d930eda17536c888e7a236f
bgood77 <48415318+bgood77@users.noreply.github.com>
2020-10-03 13:38:27 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,175 @@
+const BASE = {
+  civilizations: {
+    ALCHEMISTS: {
+      label: 'Alchemists',
+      shortName: 'Alchemists',
+      location: 1,
+      className: 'Alchemists',
+    },
+    ARCHITECTS: {
+      label: 'Architects',
+      shortName: 'Architects',
+      location: 2,
+      className: 'Architects',
+    },
+    THECHOSEN: {
+      label: 'The Chosen',
+      shortName: 'The Chosen',
+      location: 3,
+      className: 'The Chosen',
+    },
+    FUTURISTS: {
+      label: 'Futurists',
+      shortName: 'Futurists',
+      location: 4,
+      className: 'Futurists',
+    },
+    CRAFTSMEN: {
+      label: 'Craftsmen',
+      shortName: 'Craftsmen',
+      location: 5,
+      className: 'Craftsmen',
+    },
+    ENTERTAINERS: {
+      label: 'Entertainers',
+      shortName: 'Entertainers',
+      location: 6,
+      className: 'Entertainers',
+    },
+    HERALDS: {
+      label: 'Heralds',
+      shortName: 'Heralds',
+      location: 7,
+      className: 'Heralds',
+    },
+    HISTORIANS: {
+      label: 'Historians',
+      shortName: 'Historians',
+      location: 8,
+      className: 'Historians',
+    },
+    INVENTORS: {
+      label: 'Inventors',
+      shortName: 'Inventors',
+      location: 9,
+      className: 'Inventors',
+    },
+    ISOLATIONISTS: {
+      label: 'Isolationists',
+      shortName: 'Isolationists',
+      location: 10,
+      className: 'Isolationists',
+    },
+    LEADERS: {
+      label: 'Leaders',
+      shortName: 'Leaders',
+      location: 11,
+      className: 'Leaders',
+    },
+    MERRYMAKERS: {
+      label: 'Merrymakers',
+      shortName: 'Merrymakers',
+      location: 12,
+      className: 'Merrymakers',
+    },
+    MILITANTS: {
+      label: 'Militants',
+      shortName: 'Militants',
+      location: 13,
+      className: 'Militants',
+    },
+    MYSTICS: {
+      label: 'Mystics',
+      shortName: 'Mystics',
+      location: 14,
+      className: 'Mystics',
+    },
+    NOMADS: {
+      label: 'Nomads',
+      shortName: 'Nomads',
+      location: 15,
+      className: 'Nomads',
+    },
+    TRADERS: {
+      label: 'Traders',
+      shortName: 'Traders',
+      location: 16,
+      className: 'Traders',
+    },
+  },
+};
+
+const PLANSPLOYS = {
+  civilizations: {
+    ADVISORS: {
+      label: 'Advisors',
+      shortName: 'Advisors',
+      location: 17,
+      className: 'Advisors',
+    },
+    ALIENS: {
+      label: 'Aliens',
+      shortName: 'Aliens',
+      location: 18,
+      className: 'Aliens',
+    },
+    INFILTRATORS: {
+      label: 'Infiltrators',
+      shortName: 'Infiltrators',
+      location: 19,
+      className: 'Infiltrators',
+    },
+    ISLANDERS: {
+      label: 'Islanders',
+      shortName: 'Islanders',
+      location: 20,
+      className: 'Islanders',
+    },
+    RECYCLERS: {
+      label: 'Recyclers',
+      shortName: 'Recyclers',
+      location: 21,
+      className: 'Recyclers',
+    },
+    RIVERFOLK: {
+      label: 'Riverfolk',
+      shortName: 'Riverfolk',
+      location: 22,
+      className: 'Riverfolk',
+    },
+    SPIES: {
+      label: 'Spies',
+      shortName: 'Spies',
+      location: 23,
+      className: 'Spies',
+    },
+    TINKERERS: {
+      label: 'Tinkerers',
+      shortName: 'Tinkerers',
+      location: 24,
+      className: 'Tinkerers',
+    },
+    TREASURE HUNTERS: {
+      label: 'Treasure Hunters',
+      shortName: 'Treasure Hunters',
+      location: 25,
+      className: 'Treasure Hunters',
+    },
+    UTILITARIANS: {
+      label: 'Utilitarians',
+      shortName: 'Utilitarians',
+      location: 26,
+      className: 'Utilitarians',
+    },
+  },
+};
+
+const PLANSPLOYS = {
+  landmarks: [
+    'Urban Farm',
+    'TV Station',
+    'Digital Studio',
+    'Skyscraper',
+    'Game Store',
+  ],
+};
